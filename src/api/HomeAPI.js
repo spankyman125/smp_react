55bb388601985c53a4b96f155a48760b3ed04c43
spankyman125 <spankyman@live.ru>
2022-08-08 18:25:59 +0700

    HomeAPI get random items methods added

--- src/api/HomeAPI.js
+++ src/api/HomeAPI.js
@@ -1,3 +1,26 @@
-export class HomeAPI {
+import {json, status} from "./Utilities"
+import {BaseAPI} from "./BaseAPI"
+export class HomeAPI extends BaseAPI {
+  
+    static path = super.path + "/home";
+
+    static async get_random_albums(limit) {
+        return fetch(this.path + "/random" + "/albums?" + (limit? new URLSearchParams({limit: limit}) : ""))
+        .then(status)
+        .then(json)
+    }
+
+    static async get_random_songs(limit) {
+        return fetch(this.path + "/random" + "/songs?" + (limit? new URLSearchParams({limit: limit}) : ""))
+        .then(status)
+        .then(json)
+    }
+
+    static async get_random_artists(limit) {
+        return fetch(this.path + "/random" + "/artists?" + (limit? new URLSearchParams({limit: limit}) : ""))
+        .then(status)
+        .then(json)
+    }
+
 
 }
