2560477935093f1c6d2c2d9ac5a0e82136971768
spankyman125 <spankyman@live.ru>
2022-08-18 22:22:50 +0700

    Empty card grid fixed

--- src/app/components/main/left/CardsGrid.js
+++ src/app/components/main/left/CardsGrid.js
@@ -1,15 +1,10 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import {AlbumCard} from './album/AlbumCard'
 
 
-export function CardsGrid(props) {
+export function CardsGrid({ children }) {
   return (
     <Grid container spacing={2}>
-      {props.children.map((child,index)=>(<Grid item key={index}>{child}</Grid>))}
+      {children?.map((child, index) => (<Grid item key={index}>{child}</Grid>))}
     </Grid>
   );
 }
