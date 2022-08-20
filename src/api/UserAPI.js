d5a02dbb3462f4f393d4b460ac1119586deb04ca
spankyman125 <spankyman@live.ru>
2022-08-20 13:47:24 +0700

    Upload user's avatar && update user's info methods added to UserAPI

--- src/api/UserAPI.js
+++ src/api/UserAPI.js
@@ -13,26 +13,65 @@ export class UserAPI extends BaseAPI {
 
   static async get(username) {
     return fetch(this.path + `/${username}`, withAuth())
-    .then(status)
-    .then(json)
+      .then(status)
+      .then(json)
+  }
+
+  static async update(name, surname, about, email) {
+    return fetch(
+      this.path + '/me',
+      withAuth({
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: name,
+          surname: surname,
+          about: about,
+          email: email,
+        })
+      })
+    )
+      .then(status)
+      .then(json)
+  }
+
+  static async uploadImage(imageFile) {
+    const formData = new FormData()
+    formData.append("file", imageFile);
+
+    return fetch(
+      this.path + '/me/upload-image',
+      withAuth({
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+        },
+        body: formData
+      })
+    )
+      .then(status)
+      .then(json)
   }
 
   static async get_songs(username) {
     return fetch(this.path + `/${username}/songs`, withAuth())
-    .then(status)
-    .then(json)
+      .then(status)
+      .then(json)
   }
 
   static async get_albums(username) {
     return fetch(this.path + `/${username}/albums`, withAuth())
-    .then(status)
-    .then(json)
+      .then(status)
+      .then(json)
   }
 
   static async get_artists(username) {
     return fetch(this.path + `/${username}/artists`, withAuth())
-    .then(status)
-    .then(json)
+      .then(status)
+      .then(json)
   }
 
   static async create(
