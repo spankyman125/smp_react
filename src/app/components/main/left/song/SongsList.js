33fac15bd5f0662b4084c99866c1923051beaa35
spankyman125 <spankyman@live.ru>
2022-08-20 19:18:45 +0700

    ViewPortList for SongLists added

--- src/app/components/main/left/song/SongsList.js
+++ src/app/components/main/left/song/SongsList.js
@@ -1,22 +1,27 @@
 import { List } from "@mui/material";
 import { PlayerContext } from "app/contexts/PlayerContext";
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
+import ViewportList from 'react-viewport-list';
+
 
 export const SongsList = ({ songs, Render }) => {
+  const ref = useRef(null);
   const { playerContext, setPlayerContext } = useContext(PlayerContext);
   const currentSong = playerContext.queue.songs[playerContext.queue.position] || null;
 
   return (
-    <List>{
-      songs?.map((song, index) => (
-        <Render
-          key={song.id}
-          songs={songs}
-          index={index}
-          isSelected={currentSong && currentSong.id === song.id}
-          isPlaying={playerContext.isPlaying && currentSong && (currentSong.id === song.id)}
-        />
-      ))
-    }</List>
-  )
-}
+    <List  ref={ref}>
+      <ViewportList viewportRef={ref} items={songs} itemMinSize={40} margin={0} overscan={5}>
+        {(song, index) => (
+          <Render
+            key={song.id}
+            songs={songs}
+            index={index}
+            isSelected={currentSong && currentSong.id === song.id}
+            isPlaying={playerContext.isPlaying && currentSong && (currentSong.id === song.id)}
+          />
+        )}
+      </ViewportList>
+    </List>
+  );
+};
